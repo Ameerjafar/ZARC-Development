40780ce2281e2ccff2b5dd136d25eb5d0809e65a
Ameer jafar <ameerjafar123@gmail.com>
2026-01-02 16:55:39 +0530

    added the code

--- backend/main.py
+++ backend/main.py
@@ -17,7 +17,7 @@ app = FastAPI(
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[settings.FRONTEND_URL, "http://localhost:3000"],
+    allow_origins=[settings.FRONTEND_URL, "https://zarc-development.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
